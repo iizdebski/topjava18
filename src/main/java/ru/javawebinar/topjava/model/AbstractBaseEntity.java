e553cc6328951d07216bcf70d40267f97d07a564
SwAppCoder <iizdepskyy112@gmail.com>
2020-02-20 17:51:02 +0200

    9_09_js_i18n

--- src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
+++ src/main/java/ru/javawebinar/topjava/model/AbstractBaseEntity.java
@@ -3,7 +3,6 @@ package ru.javawebinar.topjava.model;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import org.hibernate.Hibernate;
 import ru.javawebinar.topjava.HasId;
-import ru.javawebinar.topjava.to.UserTo;
 
 import javax.persistence.*;
 
@@ -14,7 +13,7 @@ import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.NONE;
 // http://stackoverflow.com/questions/594597/hibernate-annotations-which-is-better-field-or-property-access
 @Access(AccessType.FIELD)
 @JsonAutoDetect(fieldVisibility= ANY, getterVisibility = NONE, isGetterVisibility = NONE,  setterVisibility = NONE)
-public abstract class AbstractBaseEntity extends UserTo implements HasId {
+public abstract class AbstractBaseEntity implements HasId {
     public static final int START_SEQ = 100000;
 
     @Id
