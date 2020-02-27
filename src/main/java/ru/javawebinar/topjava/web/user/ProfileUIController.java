db8c1383594e74c58bd88427aa91b7a83cee9f1c
SwAppCoder <iizdepskyy112@gmail.com>
2020-02-27 19:15:01 +0200

    10_09_registration

--- src/main/java/ru/javawebinar/topjava/web/user/ProfileUIController.java
+++ src/main/java/ru/javawebinar/topjava/web/user/ProfileUIController.java
@@ -1,6 +1,7 @@
 package ru.javawebinar.topjava.web.user;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,19 +17,38 @@ import javax.validation.Valid;
 public class ProfileUIController extends AbstractUserController {
 
     @GetMapping
-        public String profile() {
+    public String profile() {
         return "profile";
-        }
+    }
 
-        @PostMapping
-        public String updateProfile(@Valid UserTo userTo, BindingResult result, SessionStatus status) {
-        if(result.hasErrors()) {
+    @PostMapping
+    public String updateProfile(@Valid UserTo userTo, BindingResult result, SessionStatus status) {
+        if (result.hasErrors()) {
             return "profile";
         } else {
-        super.update(userTo, SecurityUtil.authUserId());
-        SecurityUtil.get().update(userTo);
-        status.setComplete();
-        return"redirect:meals";
+            super.update(userTo, SecurityUtil.authUserId());
+            SecurityUtil.get().update(userTo);
+            status.setComplete();
+            return "redirect:/meals";
+        }
+    }
+
+    @GetMapping("/register")
+    public String register(ModelMap model) {
+        model.addAttribute("userTo", new UserTo());
+        model.addAttribute("register", true);
+        return "profile";
+    }
+
+    @PostMapping("/register")
+    public String saveRegister(@Valid UserTo userTo, BindingResult result, SessionStatus status, ModelMap model) {
+        if (result.hasErrors()) {
+            model.addAttribute("register", true);
+            return "profile";
+        } else {
+            super.create(userTo);
+            status.setComplete();
+            return "redirect:/login?message=app.registered&username=" + userTo.getEmail();
         }
     }
 }
