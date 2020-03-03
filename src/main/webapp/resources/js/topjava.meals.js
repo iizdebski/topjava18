2e1a401fdd9f11038330baa3cd10410fe844b978
SwAppCoder <iizdepskyy112@gmail.com>
2020-03-03 17:14:48 +0200

    11_06_HW10_change_locale

--- src/main/webapp/resources/js/topjava.meals.js
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -65,6 +65,8 @@ $(function () {
         updateTable: updateFilteredTable
     });
 
+    $.datetimepicker.setLocale(localeCode);
+
     //  http://xdsoft.net/jqplugins/datetimepicker/
     var startDate = $('#startDate');
     var endDate = $('#endDate');
