0db66f224ab1d8c43046af4c42dfea8b9f3c57f5
SwAppCoder <iizdepskyy112@gmail.com>
2020-03-05 11:16:53 +0200

    11 11_restrict_modification

--- src/main/java/ru/javawebinar/topjava/util/exception/NotFoundException.java
+++ src/main/java/ru/javawebinar/topjava/util/exception/NotFoundException.java
@@ -3,8 +3,11 @@ package ru.javawebinar.topjava.util.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-public class NotFoundException extends RuntimeException {
-    public NotFoundException(String message) {
-        super(message);
+public class NotFoundException extends ApplicationException {
+    public static final String NOT_FOUND_EXCEPTION = "exception.common.notFound";
+
+    //  http://stackoverflow.com/a/22358422/548473
+    public NotFoundException(String arg) {
+        super(ErrorType.DATA_NOT_FOUND, NOT_FOUND_EXCEPTION, arg);
     }
 }
